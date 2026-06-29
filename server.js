4b6c0ac4e0748d118299aadfc4be55048a28b01a
xu.zhenzhen(许振振) <xu.zhenzhen@feg.corp>
2026-06-29 11:49:13 +0800

    更新cors配置

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const apiRouter = require('./routes/api');
 const app = express();
 const PORT = process.env.PORT || 3000;  // 优先使用平台
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://your-project-frontend-al2v4r8d1-xzz1.vercel.app/',  // 你的 Vercel 前端地址
+    'http://localhost:5173',            // 开发环境
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use('/api', apiRouter);
 
